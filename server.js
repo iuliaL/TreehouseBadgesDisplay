cab98072a34e5b00dcddd5c332dc26257bfbffb9
Iulia Maria Lungu <iulia@code11.com>
2016-10-10 15:07:35 +0300

    updated server.js to have __dirname as it should

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use('/', express.static('public'));
+app.use('/', express.static( __dirname+ '/public'));
 
 app.listen(3000,'127.0.0.1', function() {
 	console.log('Server running at http://localhost:3000');
